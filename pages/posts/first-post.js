55fc6471ddd1e350e0d7f0f568ca4c192ca639ab
rakibul1896 <rhkhan9618@gmail.com>
2022-05-16 06:13:17 +0600

    next.js app

---
+++ pages/posts/first-post.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
+
+const FirstPost = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <h1>First Post</h1>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </Layout>
+  );
+};
+export default FirstPost;
